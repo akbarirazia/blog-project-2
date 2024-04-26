291bdc9904361a0b0a328e31541b355b46a05bba
Razia Akbari <akbariharis14@gmail.com>
2024-04-26 16:10:15 +0430

    Update HeroSection.tsx

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -3,14 +3,16 @@ import SplineHero from "./SplineHero"
 
 const HeroSection = () => {
   return (
-    <motion.section
-      initial={{ opacity: 0, y: 50 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 1 }}
-      className="  w-screen flex justify-center items-center background lg:min-h-screen "
-    >
-      <SplineHero />
-    </motion.section>
+    <div className="min-h-screen w-screen flex justify-center items-center background bg-black">
+      <motion.section
+        initial={{ opacity: 0, y: 50 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1 }}
+        className="  w-screen flex justify-center items-center background lg:min-h-screen h-2/3"
+      >
+        <SplineHero />
+      </motion.section>
+    </div>
   )
 }
 
