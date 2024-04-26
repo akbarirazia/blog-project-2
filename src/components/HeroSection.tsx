dafd706a240ad8c105c0a6ce8683649c8def62b8
Razia Akbari <akbariharis14@gmail.com>
2024-04-26 13:42:43 +0430

    Add files via upload

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,25 +1,16 @@
+import { motion } from "framer-motion"
+import SplineHero from "./SplineHero"
+
 const HeroSection = () => {
   return (
-    <section className="bg-gray-200 py-16 w-screen flex justify-center items-center background h-96 ">
-      <div className="max-w-6xl mx-auto container glassmorphic-container  sm:px-6 lg:px-8">
-        <div className="lg:flex lg:items-center lg:justify-between">
-          <div className=" ">
-            <div className="mb-8">
-              <h1 className="text-4xl sm:text-5xl font-bold text-gray-800 leading-tight mb-4">
-                Welcome to <span className="text-blue-500">Humor</span>Hub
-              </h1>
-              <p className="text-xl text-gray-600 mb-8">
-                Get ready to explore fascinating stories, insightful articles,
-                and entertaining content that will blow your mind!
-              </p>
-              <button className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full">
-                <a href="#main">Start Exploring</a>
-              </button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+    <motion.section
+      initial={{ opacity: 0, y: 50 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ duration: 1 }}
+      className="  w-screen flex justify-center items-center background min-h-screen"
+    >
+      <SplineHero />
+    </motion.section>
   )
 }
 
---
+++ src/components/SplineHero.tsx
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline"
+
+export default function SplineHero() {
+  return (
+    <Spline scene="https://prod.spline.design/VZQ6nI8WbImC171z/scene.splinecode" />
+  )
+}
